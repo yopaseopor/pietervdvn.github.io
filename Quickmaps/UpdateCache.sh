9ccb7c106acf4c0a494beedb5565ffa628fe4ef9
Pieter Vander Vennet <pietervdvn@gmail.com>
2018-12-20 02:30:51 +0100

    Cache update

---
+++ Quickmaps/UpdateCache.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+function update { # search area, relId, tags
+    mkdir $1
+    cd $1
+    QUERY="[out:json][timeout:25];area($2)->.searchArea;(node$3(area.searchArea);way$3(area.searchArea);relation$3(area.searchArea););out body;>;out skel qt;"
+    wget -O "$3.json" "https://overpass-api.de/api/interpreter?data=$QUERY"
+    cd ..
+
+}
+
+cd cache
+update "Brugge" "3600562654" "['amenity'='public_bookcase']"
+update "Brugge" "3600562654" "['leisure'='nature_reserve']['operator'='Natuurpunt Brugge']"
+
