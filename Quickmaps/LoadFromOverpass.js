279833a5b198bb6ba1412746c10449fb118f8a07
Pieter Vander Vennet <pietervdvn@gmail.com>
2020-03-21 17:30:32 +0100

    Fixed caching, add belgian bookcases

--- Quickmaps/LoadFromOverpass.js
+++ Quickmaps/LoadFromOverpass.js
@@ -812,7 +812,7 @@ function CachedFirstRender(tags, searchIn, textGenerator, imageFunction, options
 		filter = filter.concat("["+tags[i]+"]");
 	}
 
- let staleQuery = "https://raw.githubusercontent.com/pietervdvn/pietervdvn.github.io/master/Quickmaps/cache/"+searchIn+"/"+encodeURIComponent(filter).replace(/~/g, '%3D')+".json"
+ let staleQuery = "https://pietervdvn.github.io/Quickmaps/cache/"+searchIn+"/"+encodeURIComponent(filter).replace(/~/g, '%3D')+".json"
     console.log("Attempting to load cache from github", staleQuery);
     $.getJSON(staleQuery, 
         function(json) {renderQuery(json.elements, textGenerator, imageFunction, options);}
